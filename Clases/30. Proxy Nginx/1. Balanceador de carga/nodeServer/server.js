ebe42300cb8ed186cf4b346de99d41bd870d0845
Mey Groppo <mariagroppo86@gmail.com>
2022-10-25 18:01:16 -0300

    Carga para consulta por configuración de nginx

---
+++ Clases/30. Proxy Nginx/1. Balanceador de carga/nodeServer/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cluster from 'cluster';
+import * as os from 'os';
+
+const modoCluster = process.argv[3] == "CLUSTER";
+const numCPUs = os.cpus().length;
+
+/* ------------------------------------------------------------------- */
+/* MASTER */
+if (modoCluster && cluster.isPrimary) {
+    console.log({numCPUs});
+    console.log(`PID MASTER ${process.pid}`);
+
+    for (let index = 0; index < numCPUs; index++) {
+        cluster.fork();        
+    }
+
+    cluster.on(`exit`, worker => {
+        console.log(`worker`, worker.process.pid, ` died`, new Date().toLocaleString());
+        cluster.fork();
+    })
+} else {
+    const app = express();
+    const PORT= parseInt(process.argv[2]) || 8080;
+
+    app.get('/datos', (req, res) => {
+        res.send(`Servidor express en ${PORT} - PID: ${process.pid} - ${new Date().toLocaleString()}`)
+    })
+    
+    app.listen(PORT, err => {
+        if (!err) console.log(`Servidor express escuchando en puerto ${PORT} - PID WORKER: ${process.pid}`)
+    })
+    
+}
